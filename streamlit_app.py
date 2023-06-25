cb6cbc6af0e4637c6e1feb7696833bd6b2241866
Ahmed-Laaziz <101425997+Ahmed-Laaziz@users.noreply.github.com>
2023-06-25 15:03:59 +0200

    Update streamlit_app.py
    Reading a file from Amazon S3 Bucket, and adding some statistics.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-  
+import pandas as pd
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('🥣Breakfast Menu')
@@ -8,3 +9,8 @@ streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Harrd-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Statistics
+# Read a file from Amazon S3 Bucket
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
