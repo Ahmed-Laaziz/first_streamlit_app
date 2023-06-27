c3ea843faa48bf72885375594110640d38b36de0
Ahmed-Laaziz <101425997+Ahmed-Laaziz@users.noreply.github.com>
2023-06-28 00:44:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,6 @@ try:
       streamlit.error('Please select a fruit to get information.')
   else:
       back_from_function = get_fruityvice_data(fruit_choice)
-      #streamlit.write('The user entered ', fruit_choice)
-      #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-      #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      # write your own comment - what does this do?
       streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
