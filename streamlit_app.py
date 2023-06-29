d767d25d2f2a950e4c65a6747749371a943e45b0
Ahmed-Laaziz <101425997+Ahmed-Laaziz@users.noreply.github.com>
2023-06-29 23:57:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 # Statistics
-# Read a file from Amazon S3 Bucket
+# Read a file from Amazon S3 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #Custom search
